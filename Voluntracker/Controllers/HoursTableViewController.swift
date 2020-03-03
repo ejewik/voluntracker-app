79d8803c5cd19b92c6dc6271cb27bf5e447ce180
Emily Jewik <emilyjewik@gmail.com>
2020-03-03 00:58:47 -0800

    Make HoursTableViewController appear in root view
    - Added table view data source and delegate as self
    - Adjusted cellForRowAt data to be dummy data
    - Added constraints to table view and enable
    programattic autolayout

--- Voluntracker/Controllers/HoursTableViewController.swift
+++ Voluntracker/Controllers/HoursTableViewController.swift
@@ -14,14 +14,30 @@ class HoursTableViewController: UIViewController, UITableViewDelegate, UITableVi
     var addEntryButton: UIButton!
     var editEntryButton: UIButton!
     
-    var hoursEntries = [HoursEntry]()
-    var tableView: HoursTableView = HoursTableView()
-    
+    var hoursEntries = [String]()
+    var hoursTableView: UITableView = HoursTableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        hoursEntries.append(HoursEntry())
-        self.tableView.register(HoursTableViewCell.self, forCellReuseIdentifier: "HoursTableViewCell")
+        prepareTableView()
+        addConstraintsToTableView()
+    }
+    
+    func prepareTableView() {
+        hoursTableView.dataSource = self
+        hoursTableView.delegate = self
+        hoursTableView.register(HoursTableViewCell.self, forCellReuseIdentifier: "HoursTableViewCell")
+        view.addSubview(hoursTableView)
+        hoursTableView.translatesAutoresizingMaskIntoConstraints = false
+        //hoursTableView.rowHeight = UITableView.automaticDimension
+        //hoursTableView.estimatedRowHeight = 44
+    }
+    
+    func addConstraintsToTableView() {
+        hoursTableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        hoursTableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        hoursTableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        hoursTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
 
 
@@ -30,15 +46,19 @@ class HoursTableViewController: UIViewController, UITableViewDelegate, UITableVi
     extension HoursTableViewController {
         
         func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return hoursEntries.count;
+            return 1;
             
         }
         
         func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             
-            var cell = tableView.dequeueReusableCell(withIdentifier: "HoursTableViewCell", for: indexPath) as! HoursTableViewCell
-            let hoursEntry = hoursEntries[indexPath.row]
-            cell = makeCellFromHoursEntry(currentCell: cell, hoursEntry: hoursEntry)
+            var cell = hoursTableView.dequeueReusableCell(withIdentifier: "HoursTableViewCell", for: indexPath) as! HoursTableViewCell
+            //let hoursEntry = hoursEntries[indexPath.row]
+            cell.hourLabel.text = "6"
+            cell.minuteLabel.text = "6"
+            cell.dateLabel.text = "6"
+            cell.organizationLabel.text = "6"
+            //cell = makeCellFromHoursEntry(currentCell: cell, hoursEntry: hoursEntry)
             return cell
         }
         
--- Voluntracker/SupportFiles/AppDelegate.swift
+++ Voluntracker/SupportFiles/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         //Setting up the rootViewController
         self.window = UIWindow(frame: UIScreen.main.bounds)
-        self.window?.backgroundColor = UIColor.green
+        self.window?.backgroundColor = UIColor.white
         self.window?.rootViewController = HoursTableViewController()
         self.window?.makeKeyAndVisible()
         return true
