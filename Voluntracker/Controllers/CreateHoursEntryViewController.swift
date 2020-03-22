79b9ddb2ee359c2cc2c18d6ec76a25739289b9e7
Emily Jewik <emilyjewik@gmail.com>
2020-03-22 16:47:40 -0700

    Embed date pickers in text fields and tweak UI
    - Embedded date, start, end pickers in text fields
    - Added borders under eventTitle and organization
    - Added extension for conversion of Date objects
    to Strings

--- Voluntracker/Controllers/CreateHoursEntryViewController.swift
+++ Voluntracker/Controllers/CreateHoursEntryViewController.swift
@@ -10,14 +10,91 @@ import UIKit
 
 class CreateHoursEntryViewController : UIViewController {
     
+    //MARK: Properties
+    
     @IBOutlet var eventTitleTextField: UITextField!
     @IBOutlet var dateTextField: UITextField!
     @IBOutlet var eventStartTimeTextField: UITextField!
     @IBOutlet var eventEndTimeTextField: UITextField!
     @IBOutlet var organizationTextField: UITextField!
     @IBOutlet var contentTextView: UITextView!
+    @IBOutlet var topLabel: UILabel!
+    @IBOutlet var middleLabel: UILabel!
+    @IBOutlet var bottomLabel: UILabel!
+    @IBOutlet var doneButton: UIButton!
     
+    var dateFieldPicker : UIDatePicker = UIDatePicker()
+    var eventStartTimePicker : UIDatePicker = UIDatePicker()
+    var eventEndTimePicker: UIDatePicker = UIDatePicker()
     
+    override func viewDidLoad() {
+        hideScrollLabels()
+        addBorderUnderTextField(textField: eventTitleTextField)
+        addBorderUnderTextField(textField: organizationTextField)
+        formatContentTextView()
+        doneButton.layer.cornerRadius = 20
+        setUpDatePickerEmbeddedInTextField()
+        setUpEventStartPickerEmbeddedInTextField()
+        setUpEventEndPickerEmbeddedInTextField()
+        
+    }
+    
+    func hideScrollLabels() {
+        topLabel.isHidden = true
+        middleLabel.isHidden = true
+        bottomLabel.isHidden = true
+    }
+    
+    func addBorderUnderTextField(textField: UITextField) {
+        let border = CALayer()
+        let width = CGFloat(1.0)
+        border.borderColor = UIColor.darkGray.cgColor
+        border.frame = CGRect(x: 0, y: textField.frame.size.height - width, width: textField.frame.size.width, height: textField.frame.size.height)
+        
+        border.borderWidth = width
+        textField.layer.addSublayer(border)
+        textField.layer.masksToBounds = true
+    }
+    
+    func formatContentTextView() {
+        contentTextView.layer.borderWidth = 0.4
+        contentTextView.layer.borderColor = UIColor.lightGray.cgColor
+        contentTextView.layer.cornerRadius = 3
+        contentTextView.clipsToBounds = true
+    }
+    
+    func setUpDatePickerEmbeddedInTextField() {
+        dateFieldPicker.datePickerMode = .date
+        dateTextField.text = dateFieldPicker.date.convertToString()
+        dateFieldPicker.addTarget(self, action: #selector(CreateHoursEntryViewController.dateChanged(datePicker:)), for: .valueChanged)
+        dateTextField.inputView = dateFieldPicker
+    }
+    
+    func setUpEventStartPickerEmbeddedInTextField() {
+        eventStartTimePicker.datePickerMode = .time
+        eventStartTimeTextField.text = eventStartTimePicker.date.convertTimeToString()
+        eventStartTimePicker.addTarget(self, action: #selector(CreateHoursEntryViewController.eventStartTimeChanged(datePicker:)), for: .valueChanged)
+        eventStartTimeTextField.inputView = eventStartTimePicker
+    }
+    
+    func setUpEventEndPickerEmbeddedInTextField() {
+        eventEndTimePicker.datePickerMode = .time
+        eventEndTimeTextField.text = eventEndTimePicker.date.convertTimeToString()
+        eventEndTimePicker.addTarget(self, action: #selector(CreateHoursEntryViewController.eventEndTimeChanged(datePicker:)), for: .valueChanged)
+        eventEndTimeTextField.inputView = eventEndTimePicker
+    }
+    
+    @objc func eventEndTimeChanged(datePicker: UIDatePicker) {
+        eventEndTimeTextField.text = eventEndTimePicker.date.convertTimeToString()
+    }
+    
+    @objc func eventStartTimeChanged(datePicker: UIDatePicker) {
+        eventStartTimeTextField.text = eventStartTimePicker.date.convertTimeToString()
+    }
+    
+    @objc func dateChanged(datePicker: UIDatePicker) {
+        dateTextField.text = datePicker.date.convertToString()
+    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let identifier = segue.identifier else { return }
@@ -28,8 +105,6 @@ class CreateHoursEntryViewController : UIViewController {
             hoursEntry.entryTitle = eventTitleTextField.text ?? ""
             hoursEntry.content = contentTextView.text ?? ""
             hoursEntry.organization = organizationTextField.text ?? ""
-            //hoursEntry.minutes = 0
-            //hoursEntry.hours = 0
             
             let destination = segue.destination as! HoursTableViewController
             destination.dummyData.append(hoursEntry)
---
+++ Voluntracker/Extensions/DateExtension.swift
@@ -0,0 +1,19 @@
+//
+//  DateHelpers.swift
+//  Voluntracker
+//
+//  Created by Emily Jewik on 3/22/20.
+//  Copyright © 2020 Emily Jewik. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    func convertToString() -> String {
+        return DateFormatter.localizedString(from: self, dateStyle: DateFormatter.Style.medium, timeStyle: DateFormatter.Style.none)
+    }
+    
+    func convertTimeToString() -> String {
+        return DateFormatter.localizedString(from: self, dateStyle: DateFormatter.Style.none, timeStyle: DateFormatter.Style.short)
+    }
+}
